935b4d705cb48b62c225f72ca328cce4e21427b3
Matea <mateabt@gmail.com>
2020-11-20 19:53:02 +0100

    uvoz knjiznic

--- lib/libraries.r
+++ lib/libraries.r
@@ -5,7 +5,12 @@ library(tidyr)
 library(tmap)
 library(shiny)
 
-options(gsubfn.engine="R")
+library("readxl")
+library("openxlsx")
+library(dplyr)
+library(readr)
+library(tidyverse)
+
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
