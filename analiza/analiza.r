c917a780ff3526020a3643fd42b4d0f5db4d6b66
Matea <mateabt@gmail.com>
2021-01-03 23:57:46 +0100

    .

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,9 +1,9 @@
 # 4. faza: Analiza podatkov
 
-#linearni model s intervalom zaupanja
+#linearni model s intervalom zaupanja neto izvoz po leti
 
 model1 <- lm(neto_izvoz ~ leto , data = pdf1)
-prihodnost <- data.frame(leto = seq(2020,2023))
+prihodnost <- data.frame(leto = seq(2020,2024))
 celoten_interval<- data.frame(leto=c(2007:2024))
 
 
@@ -14,14 +14,13 @@ tabela.napoved1 <- bind_rows( (pdf1)[c(1,4)],napoved1)
 tabela.napoved2 <- tabela.napoved1 %>% drop_na(neto_izvoz)
 
 
-lin <- transform(df, Fitted = fitted(model1))
-graf.napoved <- ggplot(tabela.napoved1, aes(x = leto, y = neto_izvoz/1e6)) + 
+
+graf.napoved <- ggplot(tabela.napoved1, aes(x = leto, y = neto_izvoz)) + 
   geom_point() + scale_x_continuous(name = "Leto", breaks = seq(1950,2024,2)) + ylab("Neto izvoz") +
-  ggtitle("Napoved gibanja neto izvoza") +
+  ggtitle("Napoved gibanja neto izvoza-linearna regresija") +
   geom_smooth(method = 'lm', formula = y ~ x,col="red") +
-  theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1))
+  theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1),
+         panel.grid.minor = element_blank())+
+  scale_y_continuous(labels=comma)
   
-
-
-
 
