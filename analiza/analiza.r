52d9a01b6bb1e837b7ec5a9abfb1b6b328b2cd7e
Matea <mateabt@gmail.com>
2021-01-03 20:16:43 +0100

    zacetek

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,3 +1,10 @@
 # 4. faza: Analiza podatkov
 
+#linearni model s intervalom zaupanja
+lregr<-ggplot(pdf1, aes(x=leto, y=neto_izvoz/1e6)) + 
+  geom_point(color='red', size = 2) + 
+  geom_smooth(method=lm,formula=y~x, color='#2C3E50')+
+  labs(title="Linearna regresija")+ylab("neto izvoz v milionih")
+
+
 
